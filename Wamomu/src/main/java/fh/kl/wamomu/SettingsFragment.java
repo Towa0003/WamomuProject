16ed6f33b39f0d0076da3594aa41bb5ed6f37481
Thundernator <paulus131313@yahoo.de>
2013-11-04 17:42:42 +0100

    Alle UI Fragments eingefügt

---
+++ Wamomu/src/main/java/fh/kl/wamomu/SettingsFragment.java
@@ -0,0 +1,24 @@
+package fh.kl.wamomu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thundernator on 04.11.13.
+ */
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings,
+                container, false);
+
+        return view;
+    }
+}
+
+
---
+++ Wamomu/src/main/java/fh/kl/wamomu/StatistikFragment.java
@@ -0,0 +1,24 @@
+package fh.kl.wamomu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thundernator on 04.11.13.
+ */
+public class StatistikFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.statistik,
+                container, false);
+
+        return view;
+    }
+}
+
+
