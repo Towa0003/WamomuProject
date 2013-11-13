7dcd92c185ee9dae1d6e524f2f43f3d7144835de
Thundernator <paulus131313@yahoo.de>
2013-11-13 19:35:45 +0100

    meals und measurement

--- Wamomu/src/main/java/fh/kl/wamomu/meta/meal.java
+++ Wamomu/src/main/java/fh/kl/wamomu/meta/meal.java
@@ -7,5 +7,47 @@ import java.text.SimpleDateFormat;
  */
 public class meal {
 
+    String foodkind; //Frühstück,Mittagessen,Abendessen
+    String food; //Schinken
+    double date;
+    double time;
 
+    public meal(String foodkind, String food, double date, double time) {
+        this.foodkind = foodkind;
+        this.food = food;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getFoodkind() {
+        return foodkind;
+    }
+
+    public void setFoodkind(String foodkind) {
+        this.foodkind = foodkind;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public double getDate() {
+        return date;
+    }
+
+    public void setDate(double date) {
+        this.date = date;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
 }
